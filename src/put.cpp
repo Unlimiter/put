90ef576b96560ae4b7b02b031a67cdf6527d4b18
Unlimiter <hichamlabidi50@gmail.com>
2019-05-30 22:55:17 +0000

    Update put.cpp

--- src/put.cpp
+++ src/put.cpp
@@ -12,9 +12,7 @@ int main(int argc, char** argv) {
   bool scanning_unit = false;
   bool is_float = false;
   bool float_error = false;
-  std::string
-    fg,
-    bg;
+  std::string fg, bg;
   while ((opt = getopt_long(argc, argv, "+:hvd:t:neE", longopts, &longindex)) != -1) {
     switch (opt) {
       case 'h':
