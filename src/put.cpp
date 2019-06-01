ffea1767adb38f41562313f8d4aa1d420d0fd680
Unlimiter <unlimiter@zoho.com>
2019-06-01 22:49:31 +0000

    Update put.cpp

--- src/put.cpp
+++ src/put.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
         exit(0);
         break;
       case 'v':
-        std::cout << "put v2.0.12-a.0\nCopyright (c) 2019 Unlimiter\n";
+        std::cout << "put v2.0.13-a.0\nCopyright (c) 2019 Unlimiter\n";
         exit(0);
         break;
       case 'd':
